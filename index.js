78afd218ae99ef5ca5792665d89bc3ebd3510ac8
zobjs <experiment.zobaidulkazi@gmail.com>
2024-08-10 21:15:27 +0600

    [nodemon] starting `node index.js`

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const { resolve } = require('path');
+
 
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
+
 
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+  res.send('Hello Kazi')
 });
 
 app.listen(port, () => {
